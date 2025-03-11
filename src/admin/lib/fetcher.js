416c89972b04427a90bfbc042b32561841662d7b
kimilguk <boramcom@daum.net>
2025-03-11 16:02:24 +0900

    코예브 클라우드  URL용으로 변경

--- src/admin/lib/fetcher.js
+++ src/admin/lib/fetcher.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 //axios.defaults.baseURL = 'http://localhost:3001';
-axios.defaults.baseURL = 'https://interior-sondra-kimilguk-app-99ae6359.koyeb.app:3001';
+axios.defaults.baseURL = 'https://interior-sondra-kimilguk-app-99ae6359.koyeb.app';
 
 const fetcher = (url) => axios.get(url).then((res) => res.data);
 
--- src/home/services/apis/mainApi.js
+++ src/home/services/apis/mainApi.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const mainApi = axios.create({
   //baseURL: 'http://localhost:3001',
-  baseURL: 'https://interior-sondra-kimilguk-app-99ae6359.koyeb.app:3001',
+  baseURL: 'https://interior-sondra-kimilguk-app-99ae6359.koyeb.app',
 });
 
 export default mainApi;

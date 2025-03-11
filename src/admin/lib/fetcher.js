c54b827577d7ac0c5faceac144dc643d3396ac95
kimilguk <boramcom@daum.net>
2025-03-11 16:34:48 +0900

    클라우드 URL 실패

--- src/admin/lib/fetcher.js
+++ src/admin/lib/fetcher.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-//axios.defaults.baseURL = 'http://localhost:4000';
-axios.defaults.baseURL = 'https://interior-sondra-kimilguk-app-99ae6359.koyeb.app';
+axios.defaults.baseURL = 'http://localhost:4000';
+//axios.defaults.baseURL = 'https://interior-sondra-kimilguk-app-99ae6359.koyeb.app';
 
 const fetcher = (url) => axios.get(url).then((res) => res.data);
 
